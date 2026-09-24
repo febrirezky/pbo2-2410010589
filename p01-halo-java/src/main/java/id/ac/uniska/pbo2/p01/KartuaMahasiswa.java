469dcbbfdbebef7ca85c0f75c270a5d2d501c934
febrirezky <febrirezky5@gmail>
2026-09-24 21:32:44 +0800

    P01: menambahkan kelas KartuMahasiswa dan screenshot output

---
+++ p01-halo-java/src/main/java/id/ac/uniska/pbo2/p01/KartuaMahasiswa.java
@@ -0,0 +1,19 @@
+package id.ac.uniska.pbo2.p01;
+
+/**
+ *
+ * @author febri
+ */
+public class KartuaMahasiswa {
+    public static void main(String[] args){
+    System.out.println("=================================");
+    System.out.println("      Kartu Mahasiwa PBO2      ");
+    System.out.println("=================================");
+    System.out.println("Nama        :   Febriawan Rezky");
+    System.out.println("NPM         :   2410010589");
+    System.out.println("Prodi       :   Teknik Informatika");
+    System.out.println("Semester    :   5    ");
+    System.out.println("Alasan      :   Ingin membuat aplikasi game edukasi    ");
+    System.out.println("=================================");
+    }
+}
